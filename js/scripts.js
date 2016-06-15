f3101d180d1443fdc0699a2dfd894535dbf8059b
Joel Delight and Patrick Lipscomb <patrick@>
2016-06-15 11:50:38 -0700

    add basic framework for drawing.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
 var draw;
 var boardDimension = 600;
 var thirdBoard = boardDimension / 3;
-
+var xClick;
+var yClick;
+var game;
 function TicTacToe() {
   this.gameState = 8000000000;
   this.playerTurn = 1;
 }
+
+
 
 TicTacToe.prototype.moveSelect = function(selectedSquare) {
   if (this.gameState.toString().charAt(10 - selectedSquare.toString().length) != "0") {
@@ -19,12 +23,12 @@ TicTacToe.prototype.moveSelect = function(selectedSquare) {
     } else if (this.playerTurn === 2) {
       this.playerTurn = 1;
     } else {console.log("Something went wrong with player turn")}
-    return this.gameState;
   };
+  console.log(this.gameState);
+  return this.gameState;
 };
 
 TicTacToe.prototype.checkForWin = function(player) {
-
   var gameStateString = this.gameState.toString();
   for (var i = 1; i < gameStateString.length; i += 3) {
     if (gameStateString.charAt(i) === gameStateString.charAt(i + 1) && gameStateString.charAt(i) === gameStateString.charAt(i + 2) && gameStateString.charAt(i) === player.toString()) {
@@ -37,14 +41,12 @@ TicTacToe.prototype.checkForWin = function(player) {
       alert("Player " + player + " wins!");
     }
   }
-
     if (gameStateString.charAt(1) === gameStateString.charAt(5) && gameStateString.charAt(1) === gameStateString.charAt(9) && gameStateString.charAt(1) === player.toString()) {
       alert("Player " + player + " wins!");
   } else if (gameStateString.charAt(3) === gameStateString.charAt(5) && gameStateString.charAt(3) === gameStateString.charAt(7) && gameStateString.charAt(3) === player.toString()) {
     alert("Player " + player + " wins!");
   }
 }
-
 
 var initBoard = function() {
   draw.moveTo(thirdBoard, 0);
@@ -59,11 +61,44 @@ var initBoard = function() {
   draw.moveTo (0, 2 * thirdBoard);
   draw.lineTo (boardDimension, 2 * thirdBoard);
   draw.stroke();
+  game = new TicTacToe();
 }
 
+var drawPiece = function(player, position) {
+  var activePlayer = 'G'
+  if (player === 1) {
+    activePlayer = "X";
+  } else if (player === 2) {
+    activePlayer = "O"
+  } else {
+    console.log('something is wrong with our clicks.')
+  }
+  draw.fillText(activePlayer, )
+}
 
-
-
+var clickPlacer = function(x, y) {
+  var clickValue = 0
+  if (x < thirdBoard) {
+    clickValue += 0;
+  } else if (x < thirdBoard * 2) {
+    clickValue += 1;
+  } else if (x < thirdBoard * 3) {
+    clickValue += 2;
+  } else {
+    console.log('sumthing is wrong with x position.')
+  }
+  if (y < thirdBoard) {
+    clickValue += 0;
+  } else if (y < thirdBoard * 2) {
+    clickValue += 3;
+  } else if (y < thirdBoard * 3) {
+    clickValue += 6;
+  } else {
+    console.log('sumthing is wrong with y position.')
+  }
+  clickValue = Math.pow(10,clickValue);
+  game.moveSelect(clickValue);
+}
 
 $(document).ready(function() {
   var canvas = document.getElementById('canvas');
@@ -72,5 +107,19 @@ $(document).ready(function() {
   canvas.height = boardDimension
 
   initBoard();
+  canvas.addEventListener("mousedown", getPosition, false);
+  function getPosition(event) {
+    var x = event.x;
+    var y = event.y;
 
+    var canvas = document.getElementById("canvas");
+
+    x -= canvas.offsetLeft;
+    y -= canvas.offsetTop;
+
+    xClick = x;
+    yClick = y;
+    clickPlacer(x, y);
+    console.log("x:" + x + " y:" + y);
+  }
 });
